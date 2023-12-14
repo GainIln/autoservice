cc8b691cf5af426f8ed971cbda2e9427294cdb92
GainIln <127286521+GainIln@users.noreply.github.com>
2023-12-15 00:18:58 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,112 @@
-let tg = window.Telegram.WebApp;
-tg.expand();
 
-tg.MainButton.textColor = "#00FF00";
-tg.MainButton.color = "#000000";
 
-let item = "";
-let frash_and_floral = document.getElementById("frash_and_floral");
-let light_and_refreshing = document.getElementById("light_and_refreshing");
-let oriental_and_exotic = document.getElementById("oriental_and_exotic");
-let confident_and_stylish = document.getElementById("confident_and_stylish");
-
-function printForm(e){
-     
-    // получаем значение поля key
-    const keyBox = document.search.key;
-    const val = keyBox.value;
-    // получаем элемент printBlock
-    const printBlock = document.getElementById("printBlock");
-    // создаем новый параграф
-    const pElement = document.createElement("p");
-    // устанавливаем у него текст
-    pElement.textContent = val;
-    // добавляем параграф в printBlock
-    printBlock.appendChild(pElement);
+body {
+	margin: 0;
+	padding: 0;
+	font-size: 18px;
+	color: var(--tg-theme-text-color);
+	background: var(--tg-theme-bg-color);
 }
- 
-const printButton = document.search.print;
-printButton.addEventListener("click", printForm);
+.container {
+	max-width: 390px;
+}
+.inner {
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	grid-template-rows: 200px 200px 200px;
+}
+.img {
+	width: 186px;
+}
 
-frash_and_floral.addEventListener("click", function () {
-	
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		window.open('https://yandex.ru');
-	}
-});
-light_and_refreshing.addEventListener("click", function () {
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Показать!");
-		item = "light_and_refreshing";
-		tg.MainButton.show();
-	}
-});
-oriental_and_exotic.addEventListener("click", function () {
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Показать!");
-		item = "oriental_and_exotic";
-		tg.MainButton.show();
-	}
-});
-confident_and_stylish.addEventListener("click", function () {
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Показать!");
-		item = "confident_and_stylish";
-		tg.MainButton.show();
-	}
-});
+p { 
+	color: #f4983d; 
+}
 
-Telegram.WebApp.onEvent("mainButtonClicked", function () {
-	tg.sendData(item);
-});
+h1 { 
+	color: #f4983d; 
+}
+
+.item {
+	text-align: center;
+}
+.button {
+	display: inline-block;
+	padding: 10px 20px;
+	border: none;
+	background: rgb(4, 107, 181);
+	border-radius: 10px;
+	color: #fff;
+	text-transform: uppercase;
+	font-weight: 700;
+	transition: background .2s linear;
+}
+.button:hover {
+	background: lightgreen;
+}
+
+
+
+.nesolestag-enadenud {
+  width: 615px;
+  margin: 0 auto;
+}
+
+.kesednsoled:after,
+.kesednsoled:before {
+  content: ".";
+  display: block;
+  overflow: hidden;
+  visibility: hidden;
+  font-size: 0;
+  line-height: 0;
+  width: 0;
+  height: 0;
+  clear: both;
+  
+}
+.ico-wrap {
+  color: #f4983d;
+  width: 48px;
+  height: 48px;
+  margin: 30px 18px 5px 5px;
+  display: block;
+  text-align: center;
+  font-size: 25px;
+  border: 2px solid #f4983d;
+  float: left;
+  border-radius: 30px;
+}
+.ico-contact {
+  line-height: 1.8 !important;
+}
+.meanous-andscoev {
+  float: left;
+  margin-bottom: 20px;
+}
+.aio-icon-title {
+  font-size: 16px;
+  color: #f4983d;
+  line-height: 1;
+  margin-bottom: 8px;
+}
+
+.media (max-width: 767px) {
+  .meanous-andscoev,
+  .ico-wrap {
+  float: none;
+  text-align: center;
+  }
+  .ico-wrap {
+  margin: 0 auto 15px auto;
+  }
+}
+
+.layer1 {
+    background: #F2EFE6;
+    border: 1px solid #f4983d;
+    padding: 10px;
+    margin: 20px;
+   }
+
+
