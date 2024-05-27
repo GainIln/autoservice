c63dc8f5bbb73c4018713d1c298e89b14c3cdfee
GainIln <127286521+GainIln@users.noreply.github.com>
2024-05-27 19:50:58 +0400

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,28 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
-let item = "";
+let listAsStr = "";
 let btn1 = document.getElementById("btn1");
 
+function calculateResult() {   
+  var totalScore = 0;
+  for (var i = 1; i <= 5; i++) {
+        var radios = document.getElementsByName('question' + i);
+        for (var j = 0, length = radios.length; j < length; j++) {
+            if (radios[j].checked) {
+                totalScore += Number(radios[j].value);
+                break;
+            }
+        }
+    }
+    if (totalScore >= 3) {
+        listAsStr='Вероятно, у вас проблемы с компьютером, обратитесь в сервис!';
+		document.getElementById("list").innerHTML = listAsStr;
+    } else {
+        listAsStr='Похоже, большинство функций вашего компьютера работают нормально.';
+		document.getElementById("list").innerHTML = listAsStr;
+    }
+}
 
 btn1.addEventListener("click", function () {
 if (tg.MainButton.isVisible) {
@@ -14,12 +33,10 @@ tg.MainButton.hide();
 }
 else {
 tg.MainButton.setText("Вывести информацию по овену");
-item = "4111";
 tg.MainButton.show();
 }
 });
 
-});
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-tg.sendData(item);
+tg.sendData(listAsStr);
 });
