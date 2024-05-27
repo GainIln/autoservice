4f87d1d6f83f750d9289c49149347032d526447a
GainIln <127286521+GainIln@users.noreply.github.com>
2024-05-27 14:26:41 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,112 +1,31 @@
-
-
-body {
-	margin: 0;
-	padding: 0;
-	font-size: 18px;
-	color: var(--tg-theme-text-color);
-	background: var(--tg-theme-bg-color);
+let tg = window.Telegram.WebApp;
+tg.expand();
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+let item = "";
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+btn1.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
 }
-.container {
-	max-width: 390px;
+else {
+tg.MainButton.setText("Вывести информацию по овену");
+item = "1";
+tg.MainButton.show();
 }
-.inner {
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	grid-template-rows: 200px 200px 200px;
+});
+btn2.addEventListener("click", function () {
+if (tg.MainButton.isVisible) {
+tg.MainButton.hide();
 }
-.img {
-	width: 186px;
+else {
+tg.MainButton.setText("Вывести информацию по тельцу");
+item = "2";
+tg.MainButton.show();
 }
-
-p { 
-	color: #f4983d; 
-}
-
-h1 { 
-	color: #f4983d; 
-}
-
-.item {
-	text-align: center;
-}
-.button {
-	display: inline-block;
-	padding: 10px 20px;
-	border: none;
-	background: rgb(4, 107, 181);
-	border-radius: 10px;
-	color: #fff;
-	text-transform: uppercase;
-	font-weight: 700;
-	transition: background .2s linear;
-}
-.button:hover {
-	background: lightgreen;
-}
-
-
-
-.nesolestag-enadenud {
-  width: 615px;
-  margin: 0 auto;
-}
-
-.kesednsoled:after,
-.kesednsoled:before {
-  content: ".";
-  display: block;
-  overflow: hidden;
-  visibility: hidden;
-  font-size: 0;
-  line-height: 0;
-  width: 0;
-  height: 0;
-  clear: both;
-  
-}
-.ico-wrap {
-  color: #f4983d;
-  width: 48px;
-  height: 48px;
-  margin: 30px 18px 5px 5px;
-  display: block;
-  text-align: center;
-  font-size: 25px;
-  border: 2px solid #f4983d;
-  float: left;
-  border-radius: 30px;
-}
-.ico-contact {
-  line-height: 1.8 !important;
-}
-.meanous-andscoev {
-  float: left;
-  margin-bottom: 20px;
-}
-.aio-icon-title {
-  font-size: 16px;
-  color: #f4983d;
-  line-height: 1;
-  margin-bottom: 8px;
-}
-
-.media (max-width: 767px) {
-  .meanous-andscoev,
-  .ico-wrap {
-  float: none;
-  text-align: center;
-  }
-  .ico-wrap {
-  margin: 0 auto 15px auto;
-  }
-}
-
-.layer1 {
-    background: #F2EFE6;
-    border: 1px solid #f4983d;
-    padding: 10px;
-    margin: 20px;
-   }
-
-
+56
+});
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+tg.sendData(item);
+});
